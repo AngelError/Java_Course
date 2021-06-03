cb2ec67602f995017c926152421e93d1e9b015b2
NabatGasimzada <ngasimzada11993@ada.edu.az>
2021-06-03 15:42:12 +0400

    Java-Course Exercises

---
+++ src/main/java/az/coders/ada_students/lessons/lesson_2/TaskNo5.java
@@ -0,0 +1,22 @@
+package az.coders.ada_students.lessons.lesson_2;
+
+import java.util.Scanner;
+
+public class TaskNo5 {
+    public static void main(String[] args) {
+        Scanner alpha = new Scanner(System.in);
+
+        System.out.println("Input an alphabet: ");
+        char Alp = alpha.next().charAt(0);
+
+        if((Alp >= 'a' && Alp <= 'z') || (Alp >= 'A' && Alp <= 'Z')) {
+            if (Alp == 'a' || Alp == 'e' || Alp == 'i' || Alp == 'o' || Alp == 'u'
+                    || Alp == 'A' || Alp == 'E' || Alp == 'I' || Alp == 'O' || Alp == 'U')
+                System.out.println("Input letter is vowel");
+            else
+                System.out.println("Input letter is consonant");
+        }
+        else
+            System.out.println("Error");
+    }
+}
---
+++ src/main/java/az/coders/ada_students/lessons/lesson_2/TaskNo5_2.java
@@ -0,0 +1,26 @@
+package az.coders.ada_students.lessons.lesson_2;
+
+import java.util.Scanner;
+
+public class TaskNo5_2 {
+        public static void main(String[] args) {
+            Scanner num = new Scanner(System.in);
+
+            System.out.println("Input an number: ");
+            int Alp = num.nextInt();
+
+            if (Alp == 1 || Alp == 2 || Alp == 3 || Alp == 4 || Alp == 0)
+                System.out.println("Suspenso");
+            else if (Alp == 7 || Alp == 8)
+                System.out.print("Notable");
+            else if (Alp == 9 || Alp == 10)
+                System.out.print("Sobresaliente");
+
+            switch (Alp) {
+                case 5:
+                    System.out.println("Aprobado");
+                case 6:
+                    System.out.print("Bien");
+            }
+        }
+}
